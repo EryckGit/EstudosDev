251c9dc4a3bc503f4f83b7f80e3182384b176039
Eryck <eryckkayck352@gmail.com>
2026-03-30 18:29:17 -0300

    LetraD.py

---
+++ Atividades Slide 4/LetraD.py
@@ -0,0 +1,10 @@
+#Algorítmo de Subtração
+
+print("Digite os dois números os quais você deseja subtrair: ")
+numero1 = (int(input("Digite o primeiro número: ")))
+numero2 = (int(input("Digite o segundo número: ")))
+
+'''Área de calculo'''
+
+resultado = numero1 - numero2
+print("O resultado da sua subtração é:",resultado)
