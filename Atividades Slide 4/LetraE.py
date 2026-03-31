6b943ca2216ada7e52a9a22eb15782a32ed657cf
Eryck <eryckkayck352@gmail.com>
2026-03-30 22:57:21 -0300

    LetraE.py

---
+++ Atividades Slide 4/LetraE.py
@@ -0,0 +1,10 @@
+#Algorítmo de IMC
+
+print("Calcule seu IMC com base no seu peso e altura")
+peso = int(input("Qual o seu peso? "))
+altura = float(input("Qual a sua altura? "))
+
+'''Calculo do IMC'''
+
+imc = peso / (altura * altura)
+print("O valor do seu imc é",imc)
