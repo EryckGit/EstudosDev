31f3a0f96b58a5741142fc85f3c061bd8d0b40de
Eryck <eryckkayck352@gmail.com>
2026-04-04 16:37:04 -0300

    Questão3.py
    Implement input and type display functionality.

---
+++ Atividades Slide 5/Questão3.py
@@ -0,0 +1,5 @@
+#Leia um valor e: Mostre o valor digitado; Mostre o tipo da variável usando type()
+
+print("Digite algo e o programa irá mostrar seu tipo!")
+variavel= input("Digite aqui sua variável: ")
+print("O tipo da sua variável é: ", type(variavel))
