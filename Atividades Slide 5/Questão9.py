0e50a866ff67e8ca81e3c8261e592efb4bc94339
Eryck <eryckkayck352@gmail.com>
2026-04-04 23:40:20 -0300

    Questão9.py
    Implementa um script que lê um valor, converte para float e exibe a metade.

---
+++ Atividades Slide 5/Questão9.py
@@ -0,0 +1,6 @@
+#Leia um valor e: Converta para float; Mostre a metade.
+
+valor = input("Digite um valor que será convertido para float e mostre metade dele: ")
+valor = float(valor)
+resultado = valor / 2
+print("O resultado foi: ",resultado, type(resultado))
