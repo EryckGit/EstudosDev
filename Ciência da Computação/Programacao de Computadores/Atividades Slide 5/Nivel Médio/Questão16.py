7c7de739878065c6143e8d2fca0dc58077000a18
Eryck <eryckkayck352@gmail.com>
2026-04-07 12:54:05 -0300

    Update Questão16.py

--- Ciência da Computação/Programacao de Computadores/Atividades Slide 5/Nivel Médio/Questão16.py
+++ Ciência da Computação/Programacao de Computadores/Atividades Slide 5/Nivel Médio/Questão16.py
@@ -1,12 +1,10 @@
 #Leia um valor e: Mostre o tipo; Se for numérico (após conversão) → mostre o quadrado.
 
 valor = input("Digite um valor: ")
-print(type(valor))
 
-'''.isdigit() serve para descrobrir se oque pessoa escreveu é númerico'''
 
-if valor.isdigit():      
+if valor >= "0" and valor <= "99999999999":
     valor = int(valor)
-    print(valor * 2)
+    print(valor*2)
 else:
-    print(False)
+    print("Oque voce digitou não é numerico.")
